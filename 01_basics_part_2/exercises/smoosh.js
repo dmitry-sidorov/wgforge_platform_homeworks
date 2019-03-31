0a70fd874cd17fd810d64731f67b91752d021da1
kvatoferma <jimmaggot@mail.ru>
2019-03-31 15:22:59 +0300

    remove commented code

--- 01_basics_part_2/exercises/smoosh.js
+++ 01_basics_part_2/exercises/smoosh.js
@@ -16,7 +16,6 @@
  */
 
 function smoosh(array) {
-  // checkArgument(array);
   const errorMessage = 'argument should be an array';
   if (!(array instanceof Array)) {
     throw { message: 'argument should be an array' };
@@ -25,16 +24,8 @@ function smoosh(array) {
   function inspect(array) {
     array.forEach((item, i) => {
       if (item instanceof Array) {
-        // console.log('=== if ===');
-        // console.log('i: ', i);
-        // console.log('item: ', item);
-        // console.log('result: ', result);
         inspect(item);
       } else {
-        // console.log('--- else ---');
-        // console.log('i: ', i);
-        // console.log('item pushed: ', item);
-        // console.log('result: ', result);
         result.push(item);
       }
     });
@@ -44,35 +35,6 @@ function smoosh(array) {
 }
 
 function squeeze(array) {
-  // checkArgument(array);
-  /*
-  function inspect(array) {
-    array.forEach((item, i) => {
-      if (item instanceof Array) {
-        inspect(array.shift(item));
-        console.log('if branch');
-        console.log('i: ', i);
-        console.log('item: ', item);
-        console.log('array: ', array);
-        // console.log('result: ', result);
-        
-      } else {
-        return item;
-        
-        
-        console.log('else branch');
-        console.log('i: ', i);
-        console.log('item pushed: ', item);
-        console.log('array: ', array);
-        // console.log('result: ', result);
-        
-      }
-      // array.unshift(result);
-      // return array;
-    });
-  }
-  inspect(array);
-  */
   let result = smoosh(array);
   const len = array.length;
   for (let i = 0; i < len; i++) {
@@ -81,18 +43,7 @@ function squeeze(array) {
   result.forEach(item => {
     array.push(item);
   });
-
   return array;
-  }
-
-
-  function argException() {
-    this.message = 'argument should be an array';
-  }
-
-// console.log(smoosh([[1,2], 3, [4, [5]]]));
-// console.log('func result: ', squeeze([[1,2], 3, [4, [5]]]));
-// console.log('func result: ', squeeze([1, [2, [3, 4]], [[5, 6], [7, [8, [9]]]]]));
+}
 
 export { smoosh, squeeze };
-// console.log(smoosh([1, [2, [3, 4]], [[5, 6], [7, [8, [9]]]]]));
